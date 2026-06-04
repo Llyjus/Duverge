0048f46240728d0ca40fdc93f52ab43ad34e6359
Llyjus <Llyjus@protonmail.com>
2026-06-04 18:03:35 +0800

    chore: fix the code

--- gateway/cmd/server/main.go
+++ gateway/cmd/server/main.go
@@ -1,13 +1,12 @@
 package main
 
-import {
-	"fmt",
-	"net/http",
-}
+import (
+	"fmt"
+	"net/http"
+)
 
 func main() {
 	fmt.Println("Gateway is running on port 26526...")
-
 
 	err := http.ListenAndServe(":26526", nil)
 	if err != nil {
