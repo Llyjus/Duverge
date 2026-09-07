0f014d983b11233eb9052a82182cdb5c8daa28bd
Llyjus <Llyjus@protonmail.com>
2026-09-07 18:44:43 +0100

    feat: add 'find user by account id'

--- userData/userdata/src/main/java/com/Duverge/userdata/repository/UserRepository.java
+++ userData/userdata/src/main/java/com/Duverge/userdata/repository/UserRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.duverge.userdata.entity.User;
 
 public interface UserRepository extends JpaRepository<User, Long> {
-    
+    public User findByAccountId(String accountId);
 }
