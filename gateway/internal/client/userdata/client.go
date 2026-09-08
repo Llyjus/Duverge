66871f438a6b166f8ec959ec81826418df44e57a
Llyjus <Llyjus@protonmail.com>
2026-09-08 14:07:54 +0100

    add grpc client

---
+++ gateway/internal/client/userdata/client.go
@@ -0,0 +1,34 @@
+package userdata
+
+import (
+	gen "gateway/proto/userdata/gen"
+
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+)
+
+type Client struct {
+	client gen.UserDataServiceClient
+	conn   *grpc.ClientConn
+}
+
+func NewClient(addr string) (*Client, error) {
+	conn, err := grpc.NewClient(
+		addr,
+		grpc.WithTransportCredentials(insecure.NewCredentials()),
+	)
+	if err != nil {
+		return nil, err
+	}
+
+	client := gen.NewUserDataServiceClient(conn)
+
+	return &Client{
+		client: client,
+		conn:   conn,
+	}, nil
+}
+
+func (c *Client) Close() {
+	c.conn.Close()
+}
