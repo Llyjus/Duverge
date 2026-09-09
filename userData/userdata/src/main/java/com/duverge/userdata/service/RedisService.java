70a677e6832ac31737d65f3dcf1d9dcfc1a814b7
Llyjus <Llyjus@protonmail.com>
2026-09-09 19:17:00 +0100

    add expiration of the session

--- userData/userdata/src/main/java/com/duverge/userdata/service/RedisService.java
+++ userData/userdata/src/main/java/com/duverge/userdata/service/RedisService.java
@@ -2,6 +2,7 @@ package com.duverge.userdata.service;
 
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Service;
+import java.time.Duration;
 
 @Service
 public class RedisService {
@@ -14,13 +15,17 @@ public class RedisService {
 
     public void setLoginSession(String sessionId, String accountId) {
         redisTemplate.opsForValue().set(sessionId, 
-                                        accountId);
+                                        accountId,
+                                        Duration.ofDays(7));
     }
 
     public String getLoginSession(String sessionId) {
         if (sessionId == null || sessionId.isEmpty()) {
             return null;
         };
+        
+        // Reset the expiration time
+        redisTemplate.expire(sessionId, Duration.ofDays(7));
         return redisTemplate.opsForValue().get(sessionId);
     }
 }
